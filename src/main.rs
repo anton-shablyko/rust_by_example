b0b51d9e6307849e43524a87e1a1581122dfc3fe
anton-shablyko <shablyko@gmail.com>
2023-02-08 18:30:43 -0800

    renaming hello_world module to p1_hello_world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod hello_world;  // https://doc.rust-lang.org/stable/rust-by-example/hello.html
+mod p1_hello_world;  // https://doc.rust-lang.org/stable/rust-by-example/hello.html
 mod p2_primitives;  //https://doc.rust-lang.org/stable/rust-by-example/primitives.html
 
 
@@ -9,13 +9,13 @@ fn main() {
 
 #[allow(dead_code)]
 fn part_1_hello_world() {
-    hello_world::hello_world::hello_world();
-    hello_world::formatted_print_mod::formated_print();
-    hello_world::debug_mod::debug_examples();
-    hello_world::display_mod::display_examples();
-    hello_world::list_print_mod::print_list();
-    hello_world::formatting_mod::formatting_example_city();
-    hello_world::formatting_mod::formatting_example_color();
+    p1_hello_world::hello_world::hello_world();
+    p1_hello_world::formatted_print_mod::formated_print();
+    p1_hello_world::debug_mod::debug_examples();
+    p1_hello_world::display_mod::display_examples();
+    p1_hello_world::list_print_mod::print_list();
+    p1_hello_world::formatting_mod::formatting_example_city();
+    p1_hello_world::formatting_mod::formatting_example_color();
 }
 
 fn part_2_primitives(){
